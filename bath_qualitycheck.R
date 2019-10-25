6d7b54f6a7941aed93de5a8c76701f1e3e7cb64f
s-akkoub <s_ak_98@gmx.de>
2019-10-25 15:10:44 +0200

    Start to clean the script

--- bath_qualitycheck.R
+++ bath_qualitycheck.R
@@ -10,145 +10,93 @@ riverData <- kwb.utils::loadObject("store_river_data.RData", objectname = "x")
 
 river <- "havel"
 
-riverData$river
-riverData[[river]]
+#request function?
 
-identical(riverData$havel, riverData[["havel"]])
+    if ((river <- "havel") == TRUE) 
+    {
+      riverData$river
+      riverData$havel$hygiene_havel
+      x <- riverData$havel
+      #rename parameters
+      x$hygiene_havel <- kwb.utils::renameColumns(x$hygiene_havel, list(
+        "datum" = "date", 
+        "e.coli" = "conc_ec",
+        "entero" = "conc_ie"
+      ))
+      x$hygiene_havel$datum <- x$hygiene_havel$datum + 12*60*60
+      write.table(x$hygiene_havel, file = "havel_temp.csv", sep = ",",
+                  row.names = FALSE, quote = FALSE)
+      
+    } else if ((river <- "rhein") == TRUE){
+      riverData$river
+      riverData$rhein$hygiene_rhein
+      x <- riverData$rhein
+      #rename parameters
+      x$hygiene_rhein <- kwb.utils::renameColumns(x$hygiene_rhein, list(
+        "datum" = "date", 
+        "e.coli" = "conc_ec",
+        "entero" = "conc_ie"
+      ))
+      x$hygiene_rhein$datum <- x$hygiene_rhein$datum + 12*60*60
+      write.table(x$hygiene_rhein, file = "rhein_temp.csv", sep = ",",
+                  row.names = FALSE, quote = FALSE)
+      
+    } else if ((river <- "mosel") == TRUE){
+      riverData$river
+      riverData$mosel$hygiene_mosel
+      x <- riverData$mosel
+      #rename parameters
+      x$hygiene_mosel <- kwb.utils::renameColumns(x$hygiene_mosel, list(
+        "datum" = "date", 
+        "e.coli" = "conc_ec",
+        "entero" = "conc_ie"
+      ))
+      x$hygiene_mosel$datum <- x$hygiene_mosel$datum + 12*60*60
+      write.table(x$hygiene_mosel, file = "mosel_temp.csv", sep = ",", 
+                  row.names = FALSE, quote = FALSE)
+      
+    } else if ((river <- "ruhr") == TRUE){
+      riverData$river
+      riverData$ruhr$hygiene_ruhr
+      x <- riverData$ruhr
+      #rename parameters
+      x$hygiene_ruhr <- kwb.utils::renameColumns(x$hygiene_ruhr, list(
+        "datum" = "date", 
+        "e.coli" = "conc_ec",
+        "entero" = "conc_ie"
+      ))
+      x$hygiene_ruhr$datum <- x$hygiene_ruhr$datum + 12*60*60
+      write.table(x$hygiene_ruhr, file = "ruhr_temp.csv", sep = ",",
+                  row.names = FALSE, quote = FALSE)
+      
+    } else if ((river <- "isar") == TRUE){
+      riverData$river
+      riverData$isar$hygiene_marzling
+      x <- riverData$isar
+      #rename parameters
+      x$hygiene_marzling <- kwb.utils::renameColumns(x$hygiene_marzling, list(
+        "datum" = "date", 
+        "e.coli" = "conc_ec",
+        "entero" = "conc_ie"
+      ))
+      x$hygiene_marzling$datum <- x$hygiene_marzling$datum + 12*60*60
+      write.table(x$hygiene_marzling, file = "isar_temp.csv", sep = ",",
+                  row.names = FALSE, quote = FALSE)
+      
+    } else if ((river <- "ilz") == TRUE){
+      riverData$river
+      riverData$ilz$hygiene_fischhaus
+      x <- riverData$ilz
+      #rename parameters
+      x$hygiene_fischhaus <- kwb.utils::renameColumns(x$hygiene_fischhaus, list(
+        "datum" = "date", 
+        "e.coli" = "conc_ec",
+        "entero" = "conc_ie"
+      ))
+      x$hygiene_fischhaus$datum <- x$hygiene_fischhaus$datum + 12*60*60
+      write.table(x$hygiene_fischhaus, file = "ilz_temp.csv", sep = ",",
+                  row.names = FALSE, quote = FALSE)
+    }
 
-riverData$havel$hygiene_havel
 
-x <- riverData$havel
-#x <- riverData$ilz
 
-#head(x$hygiene_fischhaus)
-#head(x$ka_ilz)
-#head(x$q_ilz)
-
-x$hygiene_havel$datum <- x$hygiene_havel$datum + 12*60*60
-
-x$hygiene_havel$conc_ie <- 9999
-
-write.table(
-  kwb.utils::selectElements(x, "hygiene_havel"),
-  file = "havel.csv", sep = ",", row.names = FALSE, quote = FALSE
-)
-
-# na = "NA" soll die fehlende Reihe ersetzen
-#Isar
-#river <- "isar"
-
-if (FALSE) 
-{
-  riverData$isar$hygiene_marzling
-  x <- riverData$isar
-  x$hygiene_marzling$datum <- x$hygiene_marzling$datum + 12*60*60
-  write.table(x$hygiene_marzling, file = "isar_temp.csv", row.names = FALSE)
-}  
-
-#Ilz
-river <- "ilz"
-
-riverData$river
-riverData[[river]]
-
-identical(riverData$havel, riverData[["ilz"]])
-
-riverData$ilz$hygiene_fischhaus
-
-x <- riverData$ilz
-
-#head(x$hygiene_fischhaus)
-#head(x$ka_ilz)
-#head(x$q_ilz)
-
-x$hygiene_fischhaus <- kwb.utils::renameColumns(x$hygiene_fischhaus, list(
-  "datum" = "date", 
-  "e.coli" = "conc_ec",
-  "entero" = "conc_ie"
-))
-
-x$hygiene_fischhaus$date <- x$hygiene_fischhaus$date + 12*60*60
-x$hygiene_fischhaus$conc_ie[is.na(x$hygiene_fischhaus$conc_ie)] <- 9999
-
-nrow(x$hygiene_fischhaus)
-
-blocks <- kwb.utils::splitIntoFixSizedBlocks(x$hygiene_fischhaus, blocksize = 50)
-
-for (i in seq_along(blocks)) {
-  write.table(
-    blocks[[i]],
-    file = sprintf("ilz_%d.csv", i), sep = ",", row.names = FALSE, quote = FALSE
-  )
-}
-
-#mosel
-river <- "mosel"
-
-riverData$river
-riverData[[river]]
-
-identical(riverData$mosel, riverData[["mosel"]])
-
-riverData$mosel$hygiene_mosel
-
-x <- riverData$mosel
-#x <- riverData$ilz
-
-#head(x$hygiene_fischhaus)
-#head(x$ka_ilz)
-#head(x$q_ilz)
-
-x$hygiene_mosel$datum <- x$hygiene_mosel$datum + 12*60*60
-
-write.table(
-  kwb.utils::selectElements(x, "hygiene_mosel"),
-  file = "mosel.csv", sep = ",", row.names = FALSE, quote = FALSE
-)
-
-#Rhein
-river <- "rhein"
-
-riverData$river
-riverData[[river]]
-
-identical(riverData$rhein, riverData[["rhein"]])
-
-riverData$rhein$hygiene_rhein
-
-x <- riverData$rhein
-#x <- riverData$ilz
-
-#head(x$hygiene_fischhaus)
-#head(x$ka_ilz)
-#head(x$q_ilz)
-
-x$hygiene_rhein$datum <- x$hygiene_rhein$datum + 12*60*60
-
-write.table(
-  kwb.utils::selectElements(x, "hygiene_rhein"),
-  file = "rhein.csv", sep = ",", row.names = FALSE, quote = FALSE
-)
-
-#ruhr
-river <- "ruhr"
-
-riverData$river
-riverData[[river]]
-
-identical(riverData$ruhr, riverData[["ruhr"]])
-
-riverData$ruhr$hygiene_ruhr
-
-x <- riverData$ruhr
-#x <- riverData$ilz
-
-#head(x$hygiene_fischhaus)
-#head(x$ka_ilz)
-#head(x$q_ilz)
-
-x$hygiene_ruhr$datum <- x$hygiene_ruhr$datum + 12*60*60
-
-write.table(
-  kwb.utils::selectElements(x, "hygiene_ruhr"),
-  file = "ruhr.csv", sep = ",", row.names = FALSE, quote = FALSE
-)
